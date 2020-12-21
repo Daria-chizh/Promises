36854719ab9675415925f0e7b0e2b3fd6dfaa717
Daria Ablovatskaia <daryana_chizhova@list.ru>
2020-12-21 12:22:52 +0700

    Task 1. Promises. Add app.js

--- src/GameSavingLoader.js
+++ src/GameSavingLoader.js
@@ -6,3 +6,19 @@ export default class GameSavingLoader {
     return read().then((data) => json(data));
   }
 }
+
+/*
+  задача 2 static async load() {
+    const data = await read();
+    return await json(data);
+  }
+
+ задача 2 app.js:
+(async () => {
+  try {
+    const saving = await GameSavingLoader.load();
+    } catch (err) {
+    }
+})();
+
+ */
